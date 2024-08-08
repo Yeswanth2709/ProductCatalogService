55238bd5db229af79dd321c36f286d7e6ee2ab42
Yeswanth2709 <muniyeswanthreddy@gmail.com>
2024-08-08 19:26:20 +0530

    added spring data jpa dependency

--- src/main/java/com/example/productcatalogservice/models/Product.java
+++ src/main/java/com/example/productcatalogservice/models/Product.java
@@ -1,17 +1,21 @@
 package com.example.productcatalogservice.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Data;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Product extends BaseModel {
     private String name;
     private String description;
     private Double price;
     private String imageUrl;
+    @ManyToOne(cascade = CascadeType.ALL)
     private Category category;
 }
